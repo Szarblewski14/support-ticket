fd402565e96a48f2f7e47cc85932bcfc2f9a183e
Leonardo Szarblewski <leonardo27.laurindo@gmail.com>
2025-01-28 14:56:02 -0300

    Atualizando o ticket

--- src/controllers/tickets/update.js
+++ src/controllers/tickets/update.js
@@ -2,5 +2,11 @@ export function update({ req, res, database }){
     const { id } = req.params
     const { equipment, description } = req.body
 
+    database.update("tickets", id, {
+        equipment,
+        description,
+        update_at: new Date(),
+    })
+
    return res.end()
 }
